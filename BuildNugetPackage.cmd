079c2dc1094321b18c92fcbf33afa0ba46cf125f
Mike Airey <mike.s.airey@gmail.com>
2022-01-12 23:50:08 +1100

    Added Anton-V-H's x64 build implementation.

--- BuildNugetPackage.cmd
+++ BuildNugetPackage.cmd
@@ -1,9 +1,17 @@
 @echo off
 
-@echo Building Debug and Release configurations of log4cpp and producing NuGet package for Win32 platform.
+@echo Building Debug and Release configurations of log4cpp for Win32 platform...
 
 MSBuild BuildNugetPackage.vcxproj /p:Platform=Win32 /p:Configuration=Debug
 MSBuild BuildNugetPackage.vcxproj /p:Platform=Win32 /p:Configuration=Release
+
+@echo Building Debug and Release configurations of log4cpp x64 platform...
+
+MSBuild BuildNugetPackage.vcxproj /p:Platform=x64 /p:Configuration=Debug
+MSBuild BuildNugetPackage.vcxproj /p:Platform=x64 /p:Configuration=Release
+
+@echo Producing NuGet package...
+
 pushd nuget
 nuget pack
 popd
